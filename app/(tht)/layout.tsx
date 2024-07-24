eef2180abcb88a0532cf003ee505c76b9b1fee65
Desmond Awere <desmondawere@gmail.com>
2024-07-24 17:06:13 +0100

    updated meta data

--- app/(tht)/layout.tsx
+++ app/(tht)/layout.tsx
@@ -1,13 +1,5 @@
-import type { Metadata } from "next";
-import Navbar from "../../components/tht/Navbar";
-import Footer from "../../components/tht/Footer";
-
-export const metadata: Metadata = {
-  title: "Total Health Trust Limited",
-  description:
-    "Welcome to Total Health Trust Quick facts about THT: THT was founded in 1997 and commenced operations in June 1998 THT is Nigeria's leading Health Maintenance Organisation (HMO) THT won the Nigerian Healthcare Excellence Award for HMO of the year in 2016 and 2017 THT is HMO of choice to over 300 000 members from &hellip; Continued",
-  assets: "/images/logo.png",
-};
+import Navbar from "@/components/tht/Navbar";
+import Footer from "@/components/tht/Footer";
 
 export default function RootLayout({
   children,
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import {
 } from "@/app/font/font";
 
 export const metadata: Metadata = {
-  title: "Tangerine Africa Group",
+  title: "Total Health Trust Limited",
   description:
-    "Your financial partner and so much more. Free transfers, savings, loans, insurance, pensions and many other things at your finger tips.",
-  assets: "/images/logo.svg",
+    "Welcome to Total Health Trust Quick facts about THT: THT was founded in 1997 and commenced operations in June 1998 THT is Nigeria's leading Health Maintenance Organisation (HMO) THT won the Nigerian Healthcare Excellence Award for HMO of the year in 2016 and 2017 THT is HMO of choice to over 300 000 members from &hellip; Continued",
+  assets: "/images/tht/logo.png",
 };
 
 export default function RootLayout({
